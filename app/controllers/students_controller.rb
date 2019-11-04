d6a9cd5c6e803618cc92569677f2ce5be7ec97ae
Valeria <valeria.gutsuliak@gmail.com>
2019-11-03 22:01:44 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  # before_action :set_student, only: :show
+  
   
   def index
     @students = Student.all
@@ -16,10 +16,4 @@ class StudentsController < ApplicationController
     session[:form_params] = params.inspect
     redirect_to new_student_path
   end
-
-  # private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
 end
